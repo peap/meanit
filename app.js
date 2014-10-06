dc3cb1dc2342b9b5edb416fbe4374d91c896a704
Eric Petersen <eric@huskers.unl.edu>
2014-10-06 10:03:08 -0500

    Following along some more

--- app.js
+++ app.js
@@ -3,10 +3,24 @@ angular.module('meanit', [])
 '$scope',
 function($scope){
     $scope.posts = [
-        {title: 'post 1', upvotes: 5},
-        {title: 'post 2', upvotes: 43},
-        {title: 'post 3', upvotes: 300},
-        {title: 'post 4', upvotes: 12},
-        {title: 'post 5', upvotes: 10},
+        {title: 'post 1', link: '', upvotes: 5},
+        {title: 'post 2', link: '', upvotes: 43},
+        {title: 'post 3', link: '', upvotes: 300},
+        {title: 'post 4', link: '', upvotes: 12},
+        {title: 'post 5', link: '', upvotes: -10},
     ];
+    $scope.addPost = function(){
+        var title = $scope.title;
+        var link = $scope.link;
+        if (title && link){
+            $scope.posts.push({title: title, link: link, upvotes: 0});
+            $scope.title = '';
+        }
+    };
+    $scope.downvote = function(post){
+        post.upvotes -= 1;
+    };
+    $scope.upvote = function(post){
+        post.upvotes += 1;
+    };
 }]);
