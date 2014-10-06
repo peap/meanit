42ea3f1a0055ca2b2e1e3603f9d9509493e0c562
Eric Petersen <eric@huskers.unl.edu>
2014-10-06 09:47:56 -0500

    more complex data, looping, filtering

--- app.js
+++ app.js
@@ -2,5 +2,11 @@ angular.module('meanit', [])
 .controller('MainCtrl', [
 '$scope',
 function($scope){
-    $scope.test = 'Welcome to meanit.';
+    $scope.posts = [
+        {title: 'post 1', upvotes: 5},
+        {title: 'post 2', upvotes: 43},
+        {title: 'post 3', upvotes: 300},
+        {title: 'post 4', upvotes: 12},
+        {title: 'post 5', upvotes: 10},
+    ];
 }]);
